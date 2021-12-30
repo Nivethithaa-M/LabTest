cfd2e59e8f74db78361c81ed84b781434c66c327
Nivethithaa M <nivethithaaathithan75@gmail.com>
2021-12-30 10:11:32 +0530

    Update changes

--- screens/sidebar/Display.js
+++ screens/sidebar/Display.js
@@ -1,47 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
-import {db} from '../../firebase';
+import React, { useState, useEffect } from "react";
+import { Text, View } from "react-native";
+import { db } from "../../firebase";
 const Display = () => {
+  const [messages, setMessages] = useState([]);
   useEffect(() => {
-    onDisplay();
-  })
-  const onDisplay = async () => {
-    const [messages, setMessages] = useState([]);
-    const unsubscribe = db.collection('student').onSnapshot((snapshot) => {
+    const unsubscribe = db.collection("student").onSnapshot((snapshot) => {
       let v = [];
       const data = snapshot.forEach((doc) => {
-              return {
-                ...doc.data(),
-                name: doc.data().name,
-                mobilenumber: doc.data().mobilenumber,
-                address: doc.data().address
-              };
-            });
+        return {
+          ...doc.data(),
+          name: doc.data().name,
+          mobilenumber: doc.data().mobilenumber,
+          address: doc.data().address,
+        };
+      });
 
-            newMessages.push(...data);
-            console.log('threads', data);
-            setMessages(newMessages);
-          });
-  }
-  return () => unsubscribe();
-  // return (   
-  //     <View>
-  //         {/* {value.map = ((v) => {
-  //           return (
-  //           <View>
-  //             <Text>{v.name}</Text>
-  //             <Text>{v.mobilenumber}</Text>
-  //             <Text>{v.address}</Text>
-  //             </View>)
-  //         })} */}
-  //         <Text>gedl</Text>
-  //     </View>
-  // );
+      v.push(...data);
+      alert("threads", data);
+      setMessages(v);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <View>
+      {messages.map(msg => <><Text>{msg?.name}</Text></>)}
+      <Text>gedl</Text>
+    </View>
+  );
 };
 
 export default Display;
-
-
